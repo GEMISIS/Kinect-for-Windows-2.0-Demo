617c13971918184c0d7af44c789d6ec652bf7fff
Gerald McAlister <gerald.mcalister.gem@gmail.com>
2014-03-04 11:03:20 -0500

    Added main project.

---
+++ Kinect for Windows 2.0 Demo/Kinect for Windows 2.0 Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kinect_for_Windows_2._0_Demo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
